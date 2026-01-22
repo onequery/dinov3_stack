612180cdeb5acca6f13176a374c51cd73d9122f3
heesu <assess0909@gmail.com>
2026-01-22 12:37:11 +0900

    Update evaluation script for retrieval model; change CUDA device and input/output paths

--- run_eval_ret_vits16.sh
+++ run_eval_ret_vits16.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 set -e
 set -o pipefail
 
 python eval_retrieval.py \
-    --input input/stent_split_img/test/ \
+    --input input/stent_split_img_contrast/test/ \
     --config configs_retrieval/patients.yaml \
     --model-name dinov3_vits16 \
-    --out-dir outputs/eval/pat_ret/
+    --out-dir outputs/eval/dinov3_vits16/ret/1_cls_token
